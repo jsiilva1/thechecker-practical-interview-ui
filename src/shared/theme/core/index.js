7b13f3ffb0d529574f07923a3efb6f9a9f0b9904
jsiilva1 <jsiilva@outlook.com.br>
2019-12-08 15:34:50 -0300

    Added global styles and add some assets of TheChecker Co

---
+++ src/shared/theme/core/index.js
@@ -0,0 +1,7 @@
+import { GlobalStyle } from './globalStyle';
+import { Normalize } from './normalize';
+
+export {
+  GlobalStyle,
+  Normalize,
+};
---
+++ src/shared/theme/settings/colors.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Colors = createGlobalStyle`
+  :root {
+    --color-negative-light: rgba(0, 0, 0, 0.15);
+    --color-base: #F0F0F0;
+    --color-zero: #F0F0F0;
+    --color-first: #4DC27A;
+  }
+`;
+
+export default Colors;
